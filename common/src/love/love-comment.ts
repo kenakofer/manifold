63ead3758f5a3663d67ecb66937efb7752d86120
Kenan Schaefkofer <kbitikofer@gmail.com>
2023-11-05 11:09:59 -0800

    Delete files that seem unnecessary

--- common/src/love/love-comment.ts
+++
@@ -1,7 +0,0 @@
-import { Comment } from '../comment'
-
-export type OnLover = {
-  commentType: 'lover'
-  onUserId: string
-}
-export type LoverComment = Comment<OnLover>
--- common/src/supabase/is-admin.ts
+++
@@ -1,9 +0,0 @@
-import { SupabaseClient } from '@supabase/supabase-js'
-
-export async function getIsAdmin(
-  db: SupabaseClient,
-  userId: string | undefined | null
-) {
-  const { data: is_admin } = await db.rpc('is_admin', { input_string: userId })
-  return is_admin as any as boolean
-}
