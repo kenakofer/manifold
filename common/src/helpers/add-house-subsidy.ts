75173804ba4efcd28d494a43cd114e8857555a5c
Kenan Schaefkofer <kbitikofer@gmail.com>
2023-11-06 14:36:49 -0800

    Fix compile errors

--- common/src/helpers/add-house-subsidy.ts
+++ common/src/helpers/add-house-subsidy.ts
@@ -1,85 +1,39 @@
-import * as admin from 'firebase-admin'
-import { FieldValue } from 'firebase-admin/firestore'
-
-import { CPMMContract, CPMMMultiContract } from 'common/contract'
-import { isProd } from 'shared/utils'
+import { CPMMContract, CPMMMultiContract } from '../contract'
 import {
   DEV_HOUSE_LIQUIDITY_PROVIDER_ID,
   HOUSE_LIQUIDITY_PROVIDER_ID,
-} from 'common/antes'
-import { getNewLiquidityProvision } from 'common/add-liquidity'
+} from '../antes'
+import { getNewLiquidityProvision } from '../add-liquidity'
 
-const firestore = admin.firestore()
+export const addHouseSubsidy = (contract: CPMMContract | CPMMMultiContract, amount: number) => {
+  const providerId = HOUSE_LIQUIDITY_PROVIDER_ID
 
-export const addHouseSubsidy = (contractId: string, amount: number) => {
-  return firestore.runTransaction(async (transaction) => {
-    const newLiquidityProvisionDoc = firestore
-      .collection(`contracts/${contractId}/liquidity`)
-      .doc()
-
-    const providerId = isProd()
-      ? HOUSE_LIQUIDITY_PROVIDER_ID
-      : DEV_HOUSE_LIQUIDITY_PROVIDER_ID
-
-    const contractDoc = firestore.doc(`contracts/${contractId}`)
-    const snap = await transaction.get(contractDoc)
-    const contract = snap.data() as CPMMContract | CPMMMultiContract
-
-    const { newLiquidityProvision, newTotalLiquidity, newSubsidyPool } =
-      getNewLiquidityProvision(
-        providerId,
-        amount,
-        contract,
-        newLiquidityProvisionDoc.id
-      )
-
-    transaction.update(contractDoc, {
-      subsidyPool: newSubsidyPool,
-      totalLiquidity: newTotalLiquidity,
-    } as Partial<CPMMContract>)
-
-    transaction.create(newLiquidityProvisionDoc, newLiquidityProvision)
-  })
-}
-
-export const addHouseSubsidyToAnswer = (
-  contractId: string,
-  answerId: string,
-  amount: number
-) => {
-  return firestore.runTransaction(async (transaction) => {
-    const newLiquidityProvisionDoc = firestore
-      .collection(`contracts/${contractId}/liquidity`)
-      .doc()
-
-    const providerId = isProd()
-      ? HOUSE_LIQUIDITY_PROVIDER_ID
-      : DEV_HOUSE_LIQUIDITY_PROVIDER_ID
-
-    const contractDoc = firestore.doc(`contracts/${contractId}`)
-    const snap = await transaction.get(contractDoc)
-    const contract = snap.data() as CPMMContract | CPMMMultiContract
-
-    const { newLiquidityProvision } = getNewLiquidityProvision(
+  const { newLiquidityProvision, newTotalLiquidity, newSubsidyPool } =
+    getNewLiquidityProvision(
       providerId,
       amount,
       contract,
-      newLiquidityProvisionDoc.id,
-      answerId
+      'liquidityIDhere'
     )
 
-    transaction.update(contractDoc, {
-      totalLiquidity: FieldValue.increment(amount),
-    })
+  return { newLiquidityProvision, newTotalLiquidity, newSubsidyPool }
+}
 
-    const answerDoc = firestore.doc(
-      `contracts/${contractId}/answersCpmm/${answerId}`
-    )
-    transaction.update(answerDoc, {
-      totalLiquidity: FieldValue.increment(amount),
-      subsidyPool: FieldValue.increment(amount),
-    })
+export const addHouseSubsidyToAnswer = (
+  contract: CPMMContract | CPMMMultiContract,
+  answerId: string,
+  amount: number
+) => {
+  const providerId = HOUSE_LIQUIDITY_PROVIDER_ID
 
-    transaction.create(newLiquidityProvisionDoc, newLiquidityProvision)
-  })
+
+  const { newLiquidityProvision, newTotalLiquidity, newSubsidyPool } = getNewLiquidityProvision(
+    providerId,
+    amount,
+    contract,
+    'liquidityIDhere2',
+    answerId
+  )
+
+  return { newLiquidityProvision, newTotalLiquidity, newSubsidyPool }
 }
